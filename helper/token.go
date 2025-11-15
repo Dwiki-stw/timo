5436b3f90fe31f20fa529e275f1ab1ad4267f97b
setw <dwiki.stw12@gmail.com>
2025-11-15 14:35:20 +0700

    feat(helper): add functions CreateToken and ExtractToken

---
+++ helper/token.go
@@ -0,0 +1,51 @@
+package helper
+
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserUID string `json:"user_uid"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Exp     int64  `json:"exp"`
+	jwt.RegisteredClaims
+}
+
+func CreateToken(i *Claims, jwtKey []byte) (*string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_uid": i.UserUID,
+		"name":     i.Name,
+		"email":    i.Email,
+		"exp":      i.Exp,
+	})
+
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tokenString, nil
+}
+
+func ExtractToken(tokenString string, jwtKey []byte) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return jwtKey, nil
+	}, jwt.WithValidMethods([]string{"HS256"}))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+	return claims, nil
+}
